f671ecb16b54977447b9a9de5b48987580690c78
ashmessier <ashleymess122@gmail.com>
2024-09-25 17:09:14 -0400

    did assingment

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,134 @@
-class Conversation {
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
 
-  public static void main(String[] arguments) {
-    // You will start the conversation here.
-  }
+public class Conversation {
+
+  Scanner input; // user input number of rounds 
+  Random choice; // random to grab different outputs from lists 
+
+  ArrayList<String> chat_log; // array to hold convo strings in ,
+  // response variabes
+  HashMap<String, String> chat_dict; 
+  String[] intros; // not changing by program so static 
+  String[] non_responses;
+  String[] goodbyes; 
+
+/**
+ * Constructor
+ * @param input Scanner to read number of rounds input 
+ * @param chat_log empty ArrayList to add conversation scripts to
+ * @param choice Random class to generate random indexes for lists of filler words/goodbyes 
+ * @param chat_dict HashMap (like Python dictionary) containing input keys and their corresponding output values 
+ * @param intros string list of different introduction words
+ * @param non_responses string list of different mid-convo statements
+ * @param goodbyes string list of different goodbyes
+ */
+
+public Conversation(){ // Constructor does not take any inputs 
+
+  this.input = new Scanner(System.in); // initilizes scanner 
+  this.chat_log = new ArrayList<>(); // initilizes empty list, arraylist lets be variable in size
+  this.choice = new Random(); // random class to generate indices for random response lists 
+
+  // sets all the chatbot's vocabulary 
+
+  // inputs keys/values into HashMap to access when a key is seen in the input -> print a value in the output 
+  this.chat_dict = new HashMap<>(); // initilizes dictionary (map) 
+  this.chat_dict.put("YOUR", "my");  
+  this.chat_dict.put("YOU", "i"); 
+  this.chat_dict.put("I ", "you "); 
+  this.chat_dict.put("ME", "you"); 
+  this.chat_dict.put("AM", "are");  
+  this.chat_dict.put("MY", "your");
+  
+  // sets static conversation lists to reference when no keywords are in the user input 
+  this.intros = new String[]{"oh hey man! what's up?", "Hello!!", "Hi!", "Hey.", "What's on your mind?", "Tell me something cool"}; 
+  this.non_responses = new String[]{"neato", "cool", "suure", "if you say so", "umm ok", "dude what"}; 
+  this.goodbyes = new String[]{"ok bye", "see you", "Bye!!", "gooooodbye"}; 
 }
+/**
+ * Chats with the chatbot -> asks for a number of rounds n, then accepts user input 
+ * for n rounds. If the chatbot detects a keyword in the user input, it will 
+ * respond by replacing the keyword with a different. If the chatbot does not 
+ * detect any keywords, it will print out a random, non-related response. 
+ * Chat transcript is stored in an ArrayList and is printed at the end of the conversation. 
+ */
+public void letsChat(){
+  // first we need to figure out how many rounds to chat for 
+  // ask the user for a number of rounds 
+  System.out.println("ENTER NUMBER OF ROUNDS:");
+  int num_rounds = this.input.nextInt(); // saves user input to num_rounds variable 
+
+  // picks introduction phrase 
+  int intro_index = this.choice.nextInt(this.intros.length); // random index for list 
+  String intro = this.intros[intro_index]; // ok picks random item in list for bot response 
+
+  // adds intro to chat log + prints 
+  this.chat_log.add(intro); // adds to chatlog  
+  System.out.println(intro);  // prints out intro 
+
+  // iterates over chat code for number of conversation rounds 
+  for (int i = 0; i < num_rounds; i++) { // iterates numbers of rounds 
+     Scanner text = new Scanner(System.in);  // new scanner for text (why can't I use the old one?)
+     String user_input = text.nextLine(); // gets user input 
+
+     this.chat_log.add(user_input); // adds to log 
+     user_input = user_input.toUpperCase(); // makes input uppercase to deal with not replacing words already replaced 
+    
+     // new variable, turns to true if any keywords are detected in the user input 
+      boolean yes_words = false; // to check if the user input had any keywords in it, bc will change response type  
+      // iterates over keys in HashMap to check if any are in the string 
+      for (String word: this.chat_dict.keySet()) {// ok iterate over all the keys in dictionary 
+          if (user_input.contains(word)) { // issue is replaxing words weirdly // check so not in middle 
+            user_input = user_input.replace(word, this.chat_dict.get(word)); // replace key with value
+            user_input = user_input.replace("?", "."); // also replace any ? at end with a . 
+            yes_words = true;  // changes the no_words variable to false 
+          }
+        }
+        // makes user_input (with or without replaced characters) lowercase again 
+        user_input = user_input.toLowerCase(); 
+
+        // generates random choice for bot response 
+       int randomindx = this.choice.nextInt(this.non_responses.length); // random index for list 
+       String bot_response = this.non_responses[randomindx]; // ok picks random item in list for bot response 
+
+       // if any keywords have been found, sets the bot response to the altered user input, 
+       if (yes_words){ // this would mean no substitution happened bc no question 
+         bot_response = user_input; // if replaced words, make the bot response be the modified user input
+          }
+
+        // prints out bot response - either a random unrelated phrase or an altered version of the user's last input
+        System.out.println(bot_response);
+        this.chat_log.add(bot_response); // adds to chat log
+  }
+   // ends loop over number of rounds - time to say goodbye 
+  // picks random goodbye same way picked random response 
+  int indxgoodbye = this.choice.nextInt(goodbyes.length);
+  String bot_bye = goodbyes[indxgoodbye]; // assigns string to bot_bye 
+
+  // says goodbye 
+  System.out.println(bot_bye); // prints goodbye
+  this.chat_log.add(bot_bye); // adds goodbye to list 
+
+  // prints out transcript once done iterating over number of conversations + said goodbye 
+  System.out.println(""); // prints empty space 
+  System.out.println("TRANSCRIPT: "); // prints text indicating transcript printing 
+  for (int i = 0; i < this.chat_log.size(); i++) { // iterates over strings in array and prints them 
+    System.out.println(this.chat_log.get(i)); // prints strings in log 
+}}
+
+/**
+ * Chats with chatbot by creating an instance of the Conversation class 
+ * and calling the letsChat() function
+ */
+
+public static void main(String[] args) {
+  System.out.println("Welcome to CHATBOT: ");
+  Conversation chatbot = new Conversation();  // initilize class to call 
+    // chats 
+    chatbot.letsChat(); // calls function to do that 
+}
+}
+
--- reflection.md
+++ reflection.md
@@ -5,3 +5,17 @@ Use this file to record your reflection on this assignment.
 - What are your initial impressions of Java? 
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+
+
+I worked with Lucy on the assignment. We met up on Monday to go over how we had structured our code, and work through any bugs we may have had. We had very different methods of approaching how to do the assignment, mostly pertaining to how to catch keywords. I tried to write the code in a way that could be easilly altered, which ended up being pretty difficult and coming with some unanticipated issues. In my code, to add new keywords,one would just need to add a key/value pair to the HashMap. Lucy opted to include multiple if statements to check for each keyword, which I debated doing but decided my method was fundamentally the same. 
+
+I unfortunatly was not able to attend any office hours/TA sessions (extremly busy week for senior research thesis things), but I felt like I had a strong enough grasp on the assignment to justify not going. I feel a little shakey on some of the topics from this week I anticipate seeing on the next homework, so I know I will attend hours in the future. 
+
+Initially, Java felt really uncomfortable. I have a strong Python background (and not at all in using classes), so the idea that everything in Java has to be in a class did not fully sink in before doing this assignment. I had a hard time grasping what sorts of variables should be class attributes and which I could just have as local variables. Since I have had to learn and apply many different Python packages in the past, I am pretty comfortable with looking up and implementing code, so teaching myself a lot of the syntax wasn't too difficult. I feel like I caught on much quicker than I had expected to to Java syntax. 
+
+From using both Python and Java, I can conclude that the most important part of programming is being able to verbalize what you want to do. The actual execution can be researched, but going into an assignment with a plan (psuedocode) is really important to understanding what code you are writing. I benefit from commenting on every line to make sure each line is relevent and producive to my program. 
+
+In my assignment, I found that it was much harder than I thought to replace keywords. I kept accidently replacing things cyclically, for example my -> your and then you -> I would yeild "Ir". I had to find a way to make sure the bot did not replace words it already had. I did so by capitalizing the entire string and then searching for uppercase words, and replacing them with lowercase words. This works, but does not preserve the case of the user's input. I would love to find a work-around for this, but did not have the bandwidth this week to do so. 
+
+For someone taking this class in the future - I would reccomend finding a friend to bounce ideas off in the class, as well as getting practice teaching yourself syntax online!
+
